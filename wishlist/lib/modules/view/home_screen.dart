45443e71382c48c1b55f2223ad9576d4dab7737f
Tanguy Mossion <tanguymossion@orange.fr>
2024-07-06 16:56:15 +0200

    feat: home screen with no wishlist

--- wishlist/lib/modules/view/home_screen.dart
+++ wishlist/lib/modules/view/home_screen.dart
@@ -1,10 +1,43 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:gap/gap.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:wishlist/l10n/l10n.dart';
+import 'package:wishlist/shared/theme/widgets/primary_button.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    final l10n = context.l10n;
+
+    return Scaffold(
+      body: SafeArea(
+        child: Center(
+          child: Column(
+            children: <Widget>[
+              const Gap(64),
+              SvgPicture.asset(
+                'assets/svg/no_wishlist.svg',
+              ),
+              const Gap(32),
+              Text(
+                l10n.no_wishlist,
+                style: GoogleFonts.truculenta(
+                  fontSize: 36,
+                  fontWeight: FontWeight.w500,
+                ),
+              ),
+              const Gap(16),
+              PrimaryButton(
+                text: l10n.create_button,
+                onPressed: () {},
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
