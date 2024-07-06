4f0a0640a51bef50a29efa14527c7e97c41fe4e1
Tanguy Mossion <tanguymossion@orange.fr>
2024-07-06 22:35:36 +0200

    feat: add darkGrey color

--- wishlist/lib/shared/theme/colors.dart
+++ wishlist/lib/shared/theme/colors.dart
@@ -4,4 +4,6 @@ abstract final class AppColors {
   static const Color background = Color(0xFFF7F6F0);
 
   static const Color primary = Color(0xFFF28C57);
+
+  static const Color darkGrey = Color(0xFF2C2C2C);
 }
