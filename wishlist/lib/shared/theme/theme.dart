b1f7e9cf7c3bd1206e4fe67757c20837a4f34022
Tanguy Mossion <tanguymossion@orange.fr>
2024-07-06 15:50:13 +0200

    feat: create theme for the app

---
+++ wishlist/lib/shared/theme/theme.dart
@@ -0,0 +1,8 @@
+// define a ThemeData
+import 'package:flutter/material.dart';
+import 'package:wishlist/shared/theme/colors.dart';
+
+ThemeData get theme => ThemeData(
+      fontFamily: 'Truculenta',
+      scaffoldBackgroundColor: AppColors.background,
+    );
