8e3da865b411fb6e49ccd339c575241e126c9151
Tanguy Mossion <tanguymossion@orange.fr>
2024-07-06 16:55:04 +0200

    feat: add localizations

--- wishlist/lib/app/app.dart
+++ wishlist/lib/app/app.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:wishlist/app/navigation/router.dart';
+import 'package:wishlist/l10n/l10n.dart';
 import 'package:wishlist/shared/navigation/routes.dart';
 import 'package:wishlist/shared/theme/theme.dart';
 
@@ -14,6 +16,17 @@ class MyApp extends StatelessWidget {
 
       onGenerateTitle: (context) => 'Wishlist',
 
+      localizationsDelegates: const [
+        AppLocalizations.delegate,
+        GlobalMaterialLocalizations.delegate,
+        GlobalWidgetsLocalizations.delegate,
+        GlobalCupertinoLocalizations.delegate,
+      ],
+
+      supportedLocales: const [
+        Locale('fr'),
+        Locale('en'),
+      ],
       initialRoute: AppRoutes.home.name,
       // Define a function to handle named routes in order to support
       // Flutter web url navigation and deep linking.
