ad992cb1e3b594b7ab47656953b99b3510852998
Tanguy Mossion <tanguymossion@orange.fr>
2024-07-06 16:55:31 +0200

    feat: add primary color

--- wishlist/lib/shared/theme/colors.dart
+++ wishlist/lib/shared/theme/colors.dart
@@ -2,4 +2,6 @@ import 'package:flutter/material.dart';
 
 abstract final class AppColors {
   static const Color background = Color(0xFFF7F6F0);
+
+  static const Color primary = Color(0xFFF28C57);
 }
