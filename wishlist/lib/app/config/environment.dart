fad4c3467305335a0a1607f500e15c17f4a22e84
Tanguy Mossion <tanguymossion@orange.fr>
2024-08-07 21:46:47 +0200

    fix: remove stagging from Environment enum

--- wishlist/lib/app/config/environment.dart
+++ wishlist/lib/app/config/environment.dart
@@ -1,20 +1,16 @@
 enum Environment {
   dev,
-  staging,
   prod,
 }
 
 extension MappableEnvironment on Environment {
   T map<T>({
     required T Function() dev,
-    required T Function() staging,
     required T Function() prod,
   }) {
     switch (this) {
       case Environment.dev:
         return dev();
-      case Environment.staging:
-        return staging();
       case Environment.prod:
         return prod();
     }
@@ -22,13 +18,11 @@ extension MappableEnvironment on Environment {
 
   T maybeMap<T>({
     T Function()? dev,
-    T Function()? staging,
     T Function()? prod,
     required T Function() orElse,
   }) {
     return map(
       dev: dev is T Function() ? dev : orElse,
-      staging: staging is T Function() ? staging : orElse,
       prod: prod is T Function() ? prod : orElse,
     );
   }
