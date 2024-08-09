af1076ebb76a70f7cd8d4a36df8d9a3a175d3336
Tanguy Mossion <tanguymossion@orange.fr>
2024-08-09 15:16:15 +0200

    refacto(dialogs): use showAppDialog in showCreateDialog

--- wishlist/lib/shared/widgets/dialogs/create_dialog.dart
+++ wishlist/lib/shared/widgets/dialogs/create_dialog.dart
@@ -2,68 +2,39 @@ import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:wishlist/l10n/l10n.dart';
 import 'package:wishlist/shared/theme/colors.dart';
-import 'package:wishlist/shared/theme/widgets/cancel_button.dart';
-import 'package:wishlist/shared/theme/widgets/primary_button.dart';
+import 'package:wishlist/shared/widgets/dialogs/app_dialog.dart';
 
-class _CreateDialog extends StatelessWidget {
-  const _CreateDialog();
+class _CreateDialogContent extends StatelessWidget {
+  const _CreateDialogContent();
 
   @override
   Widget build(BuildContext context) {
     final l10n = context.l10n;
     const labelFontSize = 20.0;
 
-    return AlertDialog(
-      shape: RoundedRectangleBorder(
-        borderRadius: BorderRadius.circular(16),
-      ),
-      title: Text(
-        l10n.createWishlist,
-        style: GoogleFonts.truculenta(
-          fontSize: 28,
-          fontWeight: FontWeight.w500,
-        ),
-      ),
-      content: Column(
-        mainAxisSize: MainAxisSize.min,
-        children: [
-          ListBody(
-            children: <Widget>[
-              TextField(
-                style: GoogleFonts.truculenta(fontSize: labelFontSize),
-                cursorColor: AppColors.primary,
-                autofocus: true,
-                decoration: InputDecoration(
-                  hintText: l10n.name,
-                  hintStyle: GoogleFonts.truculenta(
-                    fontSize: labelFontSize,
-                    fontStyle: FontStyle.italic,
-                  ),
-                  focusedBorder: const UnderlineInputBorder(
-                    borderSide: BorderSide(color: AppColors.primary),
-                  ),
+    return Column(
+      mainAxisSize: MainAxisSize.min,
+      children: [
+        ListBody(
+          children: <Widget>[
+            TextField(
+              style: GoogleFonts.truculenta(fontSize: labelFontSize),
+              cursorColor: AppColors.primary,
+              autofocus: true,
+              decoration: InputDecoration(
+                hintText: l10n.name,
+                hintStyle: GoogleFonts.truculenta(
+                  fontSize: labelFontSize,
+                  fontStyle: FontStyle.italic,
+                ),
+                focusedBorder: const UnderlineInputBorder(
+                  borderSide: BorderSide(color: AppColors.primary),
                 ),
               ),
-            ],
-          ),
-        ],
-      ),
-      actions: <Widget>[
-        CancelButton(
-          text: l10n.cancelButton,
-          onPressed: () {
-            Navigator.of(context).pop();
-          },
-        ),
-        PrimaryButton(
-          text: l10n.createButton,
-          onPressed: () {
-            Navigator.of(context).pop();
-          },
-          style: PrimaryButtonStyle.small,
+            ),
+          ],
         ),
       ],
-      backgroundColor: AppColors.background,
     );
   }
 }
@@ -71,28 +42,12 @@ class _CreateDialog extends StatelessWidget {
 Future<void> showCreateDialog(BuildContext context) async {
   final l10n = context.l10n;
 
-  return showGeneralDialog<void>(
-    context: context,
-    barrierDismissible: true,
-    barrierLabel: l10n.closeDialog,
-    transitionDuration: const Duration(milliseconds: 300),
-    transitionBuilder: (context, animation, secondaryAnimation, child) {
-      final curvedAnimation = CurvedAnimation(
-        parent: animation,
-        curve: Curves.easeInOut,
-      );
-      return ScaleTransition(
-        scale: curvedAnimation,
-        child: child,
-      );
-    },
-    pageBuilder: (context, animation, secondaryAnimation) {
-      return AnimatedBuilder(
-        animation: animation,
-        builder: (context, child) {
-          return const _CreateDialog();
-        },
-      );
-    },
+  return showAppDialog(
+    context,
+    title: l10n.createWishlist,
+    content: const _CreateDialogContent(),
+    confirmButtonLabel: l10n.createButton,
+    onConfirm: () {},
+    onCancel: () {},
   );
 }
