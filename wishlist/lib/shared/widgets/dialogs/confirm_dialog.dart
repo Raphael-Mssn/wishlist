fa5bcc620adc64cdfad4a93e34949d93fedc7360
Tanguy Mossion <tanguymossion@orange.fr>
2024-08-09 15:26:04 +0200

    feat(dialogs): create confirm dialog

---
+++ wishlist/lib/shared/widgets/dialogs/confirm_dialog.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+import 'package:wishlist/l10n/l10n.dart';
+import 'package:wishlist/shared/widgets/dialogs/app_dialog.dart';
+
+class _ConfirmDialogContent extends StatelessWidget {
+  const _ConfirmDialogContent({
+    required this.explanation,
+  });
+
+  final String explanation;
+
+  @override
+  Widget build(BuildContext context) {
+    return Text(explanation);
+  }
+}
+
+Future<void> showConfirmDialog(
+  BuildContext context, {
+  required String title,
+  required String explanation,
+  required void Function() onConfirm,
+}) async {
+  final l10n = context.l10n;
+
+  return showAppDialog(
+    context,
+    title: title,
+    content: _ConfirmDialogContent(
+      explanation: explanation,
+    ),
+    confirmButtonLabel: l10n.confirmDialogConfirmButtonLabel,
+    onConfirm: onConfirm,
+    onCancel: () {},
+  );
+}
