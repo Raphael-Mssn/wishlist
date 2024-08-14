ff82cee2931a2e614ad1b12dc20181c08f4b8d77
Tanguy Mossion <tanguymossion@orange.fr>
2024-08-14 23:05:08 +0200

    fix: use directly supabase global var

--- wishlist/lib/shared/infra/supabase_client_provider.dart
+++ wishlist/lib/shared/infra/supabase_client_provider.dart
@@ -1,6 +1,7 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
+import 'package:wishlist/main.dart';
 
 final supabaseClientProvider = Provider<SupabaseClient>((ref) {
-  return Supabase.instance.client;
+  return supabase;
 });
