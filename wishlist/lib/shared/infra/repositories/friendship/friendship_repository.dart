e3fdec9b06974c82012af35de291611cffed8ff4
Tanguy Mossion <tanguymossion@orange.fr>
2024-08-28 13:59:46 +0200

    feat(friends): create Friendship repository

---
+++ wishlist/lib/shared/infra/repositories/friendship/friendship_repository.dart
@@ -0,0 +1,22 @@
+import 'package:fast_immutable_collections/fast_immutable_collections.dart';
+import 'package:wishlist/shared/models/friendship/friendship.dart';
+
+abstract class FriendshipRepository {
+  Future<ISet<String>> getCurrentUserFriendsIds();
+
+  Future<ISet<String>> getCurrentUserPendingFriendsIds();
+
+  Future<ISet<String>> getCurrentUserRequestedFriendsIds();
+
+  Future<FriendshipStatus> currentUserFriendshipStatusWith(String userId);
+
+  Future<void> askFriendshipTo(String userId);
+
+  Future<void> cancelFriendshipRequest(String userId);
+
+  Future<void> acceptFriendshipWith(String userId);
+
+  Future<void> declineFriendshipWith(String userId);
+
+  Future<void> removeFriendshipWith(String userId);
+}
--- wishlist/lib/shared/infra/repositories/user/user_repository.dart
+++ wishlist/lib/shared/infra/repositories/user/user_repository.dart
@@ -1,6 +1,10 @@
+import 'package:fast_immutable_collections/fast_immutable_collections.dart';
+import 'package:wishlist/shared/models/app_user.dart';
 import 'package:wishlist/shared/models/profile.dart';
 
 abstract class UserRepository {
   String getCurrentUserEmail();
   Future<void> createUserProfile(Profile profile);
+  Future<IList<AppUser>> searchUsersByEmailOrPseudo(String query);
+  Future<AppUser> getAppUserById(String userId);
 }
