f6147e4b1e460c69eb9b6f2b5700e302b458552b
Tanguy Mossion <tanguymossion@orange.fr>
2024-08-30 11:46:56 +0200

    feat(gen): use flutter_gen to generate assets

---
+++ wishlist/lib/gen/assets.gen.dart
@@ -0,0 +1,41 @@
+/// GENERATED CODE - DO NOT MODIFY BY HAND
+/// *****************************************************
+///  FlutterGen
+/// *****************************************************
+
+// coverage:ignore-file
+// ignore_for_file: type=lint
+// ignore_for_file: directives_ordering,unnecessary_import,implicit_dynamic_list_literal,deprecated_member_use
+
+class $EnvGen {
+  const $EnvGen();
+
+  /// File path: env/.env.dev
+  String get aEnvDev => 'env/.env.dev';
+
+  /// File path: env/.env.prod
+  String get aEnvProd => 'env/.env.prod';
+
+  /// List of all assets
+  List<String> get values => [aEnvDev, aEnvProd];
+}
+
+class $AssetsSvgGen {
+  const $AssetsSvgGen();
+
+  /// File path: assets/svg/no_friend.svg
+  String get noFriend => 'assets/svg/no_friend.svg';
+
+  /// File path: assets/svg/no_wishlist.svg
+  String get noWishlist => 'assets/svg/no_wishlist.svg';
+
+  /// List of all assets
+  List<String> get values => [noFriend, noWishlist];
+}
+
+class Assets {
+  Assets._();
+
+  static const $AssetsSvgGen svg = $AssetsSvgGen();
+  static const $EnvGen env = $EnvGen();
+}
