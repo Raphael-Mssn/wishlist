d6749f2e7ccdf1a14cbc92cf7ff3a98a92b82315
Tanguy Mossion <tanguymossion@orange.fr>
2024-09-02 11:34:30 +0200

    refacto(PageLayout): pass onRefresh in parameter and add AppRefreshIndicator if not necessary

--- wishlist/lib/modules/friends/view/friends_screen.dart
+++ wishlist/lib/modules/friends/view/friends_screen.dart
@@ -7,7 +7,6 @@ import 'package:wishlist/modules/friends/view/widgets/friend_pill.dart';
 import 'package:wishlist/modules/friends/view/widgets/user_pill.dart';
 import 'package:wishlist/shared/infra/friendships_provider.dart';
 import 'package:wishlist/shared/models/app_user.dart';
-import 'package:wishlist/shared/theme/widgets/app_refresh_indicator.dart';
 import 'package:wishlist/shared/widgets/page_layout.dart';
 import 'package:wishlist/shared/widgets/page_layout_empty.dart';
 
@@ -80,42 +79,40 @@ class FriendsScreen extends ConsumerWidget {
             )
           : PageLayout(
               title: l10n.fiendsScreenTitle,
-              child: AppRefreshIndicator(
-                onRefresh: refreshFriends,
-                child: ListView.builder(
-                  itemCount: data.totalCount,
-                  itemBuilder: (context, index) {
-                    final combinedList = _buildCombinedList(data);
-                    final item = combinedList[index];
-                    final isLastItem = index == combinedList.length - 1;
+              onRefresh: refreshFriends,
+              child: ListView.builder(
+                itemCount: data.totalCount,
+                itemBuilder: (context, index) {
+                  final combinedList = _buildCombinedList(data);
+                  final item = combinedList[index];
+                  final isLastItem = index == combinedList.length - 1;
 
-                    return Padding(
-                      padding: EdgeInsets.only(
-                        bottom: isLastItem ? 48 : 8,
-                      ),
-                      child: Builder(
-                        builder: (context) {
-                          switch (item.type) {
-                            case requestedType:
-                              return UserPill(
-                                appUser: item.user,
-                                isRequest: true,
-                              );
-                            case pendingType:
-                              return UserPill(
-                                appUser: item.user,
-                              );
-                            case friendType:
-                              return FriendPill(appUser: item.user);
-                            default:
-                              return const SizedBox
-                                  .shrink(); // Should never happen
-                          }
-                        },
-                      ),
-                    );
-                  },
-                ),
+                  return Padding(
+                    padding: EdgeInsets.only(
+                      bottom: isLastItem ? 48 : 8,
+                    ),
+                    child: Builder(
+                      builder: (context) {
+                        switch (item.type) {
+                          case requestedType:
+                            return UserPill(
+                              appUser: item.user,
+                              isRequest: true,
+                            );
+                          case pendingType:
+                            return UserPill(
+                              appUser: item.user,
+                            );
+                          case friendType:
+                            return FriendPill(appUser: item.user);
+                          default:
+                            return const SizedBox
+                                .shrink(); // Should never happen
+                        }
+                      },
+                    ),
+                  );
+                },
               ),
             ),
     );
--- wishlist/lib/shared/widgets/page_layout.dart
+++ wishlist/lib/shared/widgets/page_layout.dart
@@ -1,23 +1,33 @@
 import 'package:flutter/material.dart';
 import 'package:wishlist/shared/theme/widgets/app_app_bar.dart';
+import 'package:wishlist/shared/theme/widgets/app_refresh_indicator.dart';
 
 class PageLayout extends StatelessWidget {
   const PageLayout({
     super.key,
     required this.title,
     required this.child,
+    this.onRefresh,
   });
 
   final String title;
   final Widget child;
+  final Future<void> Function()? onRefresh;
 
   @override
   Widget build(BuildContext context) {
+    final onRefresh = this.onRefresh;
+
     return Scaffold(
       appBar: AppAppBar(title: title),
       body: Padding(
         padding: const EdgeInsets.all(20),
-        child: child,
+        child: onRefresh != null
+            ? AppRefreshIndicator(
+                onRefresh: onRefresh,
+                child: child,
+              )
+            : child,
       ),
     );
   }
