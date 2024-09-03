2da42e38f7f11a0875d47f436e0bfea56a44913e
Tanguy Mossion <tanguymossion@orange.fr>
2024-09-03 10:24:44 +0200

    feat(errors): create ScaffoldMessengerExtension with showTopSnackBar and showGenericError

---
+++ wishlist/lib/shared/infra/utils/scaffold_messenger_extension.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+import 'package:wishlist/l10n/l10n.dart';
+
+extension ScaffoldMessengerExtension on ScaffoldMessengerState {
+  void showTopSnackBar(
+    Widget content,
+  ) {
+    final snackBar = SnackBar(
+      content: content,
+      behavior: SnackBarBehavior.floating,
+      margin: EdgeInsets.only(
+        bottom: MediaQuery.of(context).size.height -
+            MediaQuery.of(context).viewPadding.top -
+            130,
+        left: 10,
+        right: 10,
+      ),
+    );
+
+    // Remove the current snackbar if any, then show the new one
+    removeCurrentSnackBar();
+
+    showSnackBar(snackBar);
+  }
+
+  void showGenericError(
+    BuildContext context, {
+    bool isTopSnackBar = false,
+  }) {
+    WidgetsBinding.instance.addPostFrameCallback((_) {
+      final content = Text(context.l10n.genericError);
+
+      if (isTopSnackBar) {
+        showTopSnackBar(content);
+      } else {
+        showSnackBar(SnackBar(content: content));
+      }
+    });
+  }
+}
