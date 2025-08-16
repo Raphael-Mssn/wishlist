f75b42ad9c4efe5c1b2e67f1dd5c76243612cd61
Tanguy Mossion <tanguy.mossion@theodo.com>
2025-08-16 08:53:00 +0200

    feat: add wave pattern to BaseAvatarPill

--- wishlist/lib/modules/friends/view/widgets/base_avatar_pill.dart
+++ wishlist/lib/modules/friends/view/widgets/base_avatar_pill.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:pattern_box/pattern_box.dart';
 import 'package:wishlist/shared/theme/colors.dart';
+import 'package:wishlist/shared/theme/widgets/angled_wave_painter.dart';
+import 'package:wishlist/shared/theme/widgets/rotatable_pattern_box.dart';
 import 'package:wishlist/shared/widgets/avatar/app_avatar.dart';
 
 class BaseAvatarPill extends ConsumerWidget {
@@ -29,42 +32,60 @@ class BaseAvatarPill extends ConsumerWidget {
     return InkWell(
       onTap: () => onTap(context),
       borderRadius: borderRadius,
+      highlightColor: AppColors.darken(backgroundColor),
       child: Ink(
         decoration: BoxDecoration(
-          color: backgroundColor,
           borderRadius: borderRadius,
+          color: backgroundColor,
         ),
-        child: Row(
-          children: <Widget>[
-            Container(
-              height: avatarSize,
-              width: avatarSize,
-              decoration: BoxDecoration(
-                color: AppColors.makara,
-                shape: BoxShape.circle,
-                border: Border.fromBorderSide(
-                  BorderSide(
-                    color: avatarBorderColor,
-                    width: 6,
+        child: PatternBoxWidget(
+          height: avatarSize,
+          pattern: WavePainter(
+            frequency: 0.8,
+            thickness: 12,
+            gap: 42,
+            color: AppColors.lighten(backgroundColor),
+            amplitude: 80,
+          )
+              .withAngleVariation(
+                angleVariation: 0.2,
+              )
+              .rotatedDegrees(
+                45,
+              ),
+          borderRadius: borderRadius,
+          child: Row(
+            children: <Widget>[
+              Container(
+                height: avatarSize,
+                width: avatarSize,
+                decoration: BoxDecoration(
+                  color: AppColors.makara,
+                  shape: BoxShape.circle,
+                  border: Border.fromBorderSide(
+                    BorderSide(
+                      color: avatarBorderColor,
+                      width: 6,
+                    ),
                   ),
                 ),
-              ),
-              child: AppAvatar(
-                avatarUrl: avatarUrl,
-                size: avatarSize - 12,
-                hasBorders: false,
-              ),
-            ),
-            Expanded(
-              child: Padding(
-                padding: const EdgeInsets.symmetric(
-                  horizontal: 12,
-                  vertical: 8,
+                child: AppAvatar(
+                  avatarUrl: avatarUrl,
+                  size: avatarSize - 12,
+                  hasBorders: false,
                 ),
-                child: child,
               ),
-            ),
-          ],
+              Expanded(
+                child: Padding(
+                  padding: const EdgeInsets.symmetric(
+                    horizontal: 12,
+                    vertical: 8,
+                  ),
+                  child: child,
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
