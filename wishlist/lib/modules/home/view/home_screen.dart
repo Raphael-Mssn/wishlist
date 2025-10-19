fff5a2749b1a5402f821200f4c807f331b5fe3bb
Raphael-Mssn <raphael.mossion@orange.fr>
2025-10-19 19:10:51 +0200

    feat(bookedWishesScreen): enhance navigation and layout with settings and booked wishes screens

--- wishlist/lib/modules/home/view/home_screen.dart
+++ wishlist/lib/modules/home/view/home_screen.dart
@@ -3,6 +3,7 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:wishlist/gen/assets.gen.dart';
 import 'package:wishlist/l10n/l10n.dart';
 import 'package:wishlist/shared/infra/wishlists_realtime_provider.dart';
+import 'package:wishlist/shared/navigation/routes.dart';
 import 'package:wishlist/shared/page_layout_empty/page_layout_empty.dart';
 import 'package:wishlist/shared/utils/app_snackbar.dart';
 import 'package:wishlist/shared/widgets/dialogs/create_dialog.dart';
@@ -41,11 +42,24 @@ class HomeScreen extends ConsumerWidget {
                 showCreateDialog(context, ref);
               },
               onRefresh: refreshWishlists,
+              appBarTitle: l10n.myWishlists,
+              actions: [
+                IconButton(
+                  icon: const Icon(Icons.settings),
+                  onPressed: () => SettingsRoute().push(context),
+                ),
+              ],
             )
           : PageLayout(
               padding: const EdgeInsets.all(20).copyWith(bottom: 0),
               title: l10n.myWishlists,
               onRefresh: refreshWishlists,
+              actions: [
+                IconButton(
+                  icon: const Icon(Icons.settings),
+                  onPressed: () => SettingsRoute().push(context),
+                ),
+              ],
               child: WishlistsGrid(
                 wishlists: data,
                 isReorderable: true,
--- wishlist/lib/shared/widgets/page_layout.dart
+++ wishlist/lib/shared/widgets/page_layout.dart
@@ -9,19 +9,24 @@ class PageLayout extends StatelessWidget {
     required this.child,
     this.onRefresh,
     this.padding = const EdgeInsets.all(20),
+    this.actions,
   });
 
   final String title;
   final Widget child;
   final Future<void> Function()? onRefresh;
   final EdgeInsets padding;
+  final List<Widget>? actions;
 
   @override
   Widget build(BuildContext context) {
     final onRefresh = this.onRefresh;
 
     return Scaffold(
-      appBar: AppAppBar(title: title),
+      appBar: AppAppBar(
+        title: title,
+        actions: actions,
+      ),
       body: Padding(
         padding: padding,
         child: onRefresh != null
