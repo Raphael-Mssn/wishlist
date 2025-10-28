b783f481a565159305198be7121f79283212d28a
Tanguy Mossion <tanguy.mossion@theodo.com>
2025-10-28 08:28:20 +0100

    refactor: remove old non-Realtime providers
    - Remove wishlists_provider.dart (replaced by wishlistsRealtimeProvider)
    - Remove friendships_provider.dart (replaced by friendshipsRealtimeProvider)
    - Remove friend_details_provider.dart (replaced by friendDetailsRealtimeProvider)
    - Remove wishs_from_wishlist_provider.dart (replaced by wish_streams_providers)
    - Remove wishlist_by_id_provider.dart (replaced by watchWishlistByIdProvider)
    - Remove wishlist_screen_data_provider.dart (replaced by Realtime version)

--- wishlist/lib/shared/infra/wishlist_by_id_provider.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:wishlist/shared/infra/wishlist_service.dart';
-import 'package:wishlist/shared/models/wishlist/wishlist.dart';
-
-final wishlistByIdProvider =
-    FutureProvider.family<Wishlist, int>((ref, wishlistId) async {
-  final wishlistService = ref.read(wishlistServiceProvider);
-  return wishlistService.getWishlistById(wishlistId);
-});
