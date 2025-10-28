67585a02d919026d0f0a452e7678bc48300b29e0
Tanguy Mossion <tanguy.mossion@theodo.com>
2025-10-28 08:19:58 +0100

    feat(realtime): add high-level Realtime providers
    - Create friendshipsRealtimeProvider combining friendship IDs and user details
    - Create friendDetailsRealtimeProvider for friend profile + wishlists
    - Create wishlistScreenDataRealtimeProvider combining wishlist + wishes
    - Use RxDart for combining multiple streams

---
+++ wishlist/lib/modules/wishlists/infra/wishlist_screen_data_realtime_provider.dart
@@ -0,0 +1,27 @@
+import 'package:async_value_group/async_value_group.dart';
+import 'package:fast_immutable_collections/fast_immutable_collections.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:wishlist/shared/infra/repositories/wish/wish_streams_providers.dart';
+import 'package:wishlist/shared/infra/repositories/wishlist/wishlist_streams_providers.dart';
+import 'package:wishlist/shared/models/wish/wish.dart';
+import 'package:wishlist/shared/models/wishlist/wishlist.dart';
+
+/// Version Realtime du wishlistScreenDataProvider
+///
+/// Combine les données de la wishlist et ses wishs en temps réel
+final wishlistScreenDataRealtimeProvider =
+    Provider.family<AsyncValue<WishlistScreenData>, int>((ref, wishlistId) {
+  final wishlist = ref.watch(watchWishlistByIdProvider(wishlistId));
+  final wishs = ref.watch(watchWishsFromWishlistProvider(wishlistId));
+
+  final wishlistScreenData = AsyncValueGroup.group2(wishlist, wishs);
+
+  return wishlistScreenData;
+});
+
+typedef WishlistScreenData = (Wishlist? wishlist, IList<Wish> wishs);
+
+extension WishlistScreenDataGetters on WishlistScreenData {
+  Wishlist? get wishlist => $1;
+  IList<Wish> get wishs => $2;
+}
