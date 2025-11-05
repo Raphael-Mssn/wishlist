3c610a952e0c447a7bed06abf97f294060b17794
Raphael-Mssn <raphael.mossion@orange.fr>
2025-11-05 21:48:46 +0100

    fix(friendsScreen): handle error display in a dedicated method

--- wishlist/lib/modules/friends/view/screens/friends_screen.dart
+++ wishlist/lib/modules/friends/view/screens/friends_screen.dart
@@ -52,6 +52,14 @@ class FriendsScreen extends ConsumerWidget {
     return combinedList;
   }
 
+  void _onError(BuildContext context) {
+    WidgetsBinding.instance.addPostFrameCallback((_) {
+      if (context.mounted) {
+        showGenericError(context, isTopSnackBar: true);
+      }
+    });
+  }
+
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     final l10n = context.l10n;
@@ -69,7 +77,7 @@ class FriendsScreen extends ConsumerWidget {
         child: CircularProgressIndicator(),
       ),
       error: (error, stackTrace) {
-        showGenericError(context, isTopSnackBar: true);
+        _onError(context);
         return const SizedBox.shrink();
       },
       data: (data) => data.isEmpty
