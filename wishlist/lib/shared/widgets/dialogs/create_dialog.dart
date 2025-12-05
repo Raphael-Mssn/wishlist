0783fb50ba77d07fde09c54dd6e6e249aa6435cf
Tanguy Mossion <tanguy.mossion@theodo.com>
2025-12-05 18:56:30 +0100

    Revert "feat: add visibility toggle on create dialog"
    This reverts commit 8d83e7b9d3715ea85db40112e28281e4b464e6e9.

--- wishlist/lib/shared/widgets/dialogs/create_dialog.dart
+++ wishlist/lib/shared/widgets/dialogs/create_dialog.dart
@@ -1,9 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:gap/gap.dart';
 import 'package:wishlist/gen/fonts.gen.dart';
 import 'package:wishlist/l10n/l10n.dart';
-import 'package:wishlist/modules/wishlists/view/widgets/wishlist_toggle_switch.dart';
 import 'package:wishlist/shared/infra/non_null_extensions/go_true_client_non_null_getter_user_extension.dart';
 import 'package:wishlist/shared/infra/supabase_client_provider.dart';
 import 'package:wishlist/shared/infra/wishlist_mutations_provider.dart';
@@ -13,12 +11,8 @@ import 'package:wishlist/shared/theme/colors.dart';
 import 'package:wishlist/shared/widgets/dialogs/app_dialog.dart';
 
 class _CreateDialogContent extends StatefulWidget {
-  const _CreateDialogContent({
-    required this.nameController,
-    required this.visibilityNotifier,
-  });
+  const _CreateDialogContent({required this.nameController});
   final TextEditingController nameController;
-  final ValueNotifier<bool> visibilityNotifier;
 
   @override
   State<_CreateDialogContent> createState() => _CreateDialogContentState();
@@ -26,67 +20,39 @@ class _CreateDialogContent extends StatefulWidget {
 
 class _CreateDialogContentState extends State<_CreateDialogContent> {
   @override
-  void initState() {
-    super.initState();
-    widget.visibilityNotifier.value = false;
-  }
-
-  @override
   Widget build(BuildContext context) {
     final l10n = context.l10n;
     const labelFontSize = 20.0;
 
-    return SizedBox(
-      width: double.maxFinite,
-      child: Column(
-        mainAxisSize: MainAxisSize.min,
-        crossAxisAlignment: CrossAxisAlignment.stretch,
-        children: [
-          TextField(
-            controller: widget.nameController,
-            style: const TextStyle(
-              fontFamily: FontFamily.truculenta,
-              fontSize: labelFontSize,
-            ),
-            textCapitalization: TextCapitalization.sentences,
-            cursorColor: AppColors.primary,
-            autofocus: true,
-            decoration: InputDecoration(
-              hintText: l10n.name,
-              hintStyle: const TextStyle(
+    return Column(
+      mainAxisSize: MainAxisSize.min,
+      children: [
+        ListBody(
+          children: <Widget>[
+            TextField(
+              controller: widget.nameController,
+              style: const TextStyle(
                 fontFamily: FontFamily.truculenta,
                 fontSize: labelFontSize,
-                fontStyle: FontStyle.italic,
               ),
-              focusedBorder: const UnderlineInputBorder(
-                borderSide: BorderSide(color: AppColors.primary),
+              textCapitalization: TextCapitalization.sentences,
+              cursorColor: AppColors.primary,
+              autofocus: true,
+              decoration: InputDecoration(
+                hintText: l10n.name,
+                hintStyle: const TextStyle(
+                  fontFamily: FontFamily.truculenta,
+                  fontSize: labelFontSize,
+                  fontStyle: FontStyle.italic,
+                ),
+                focusedBorder: const UnderlineInputBorder(
+                  borderSide: BorderSide(color: AppColors.primary),
+                ),
               ),
             ),
-          ),
-          const Gap(16),
-          ValueListenableBuilder<bool>(
-            valueListenable: widget.visibilityNotifier,
-            builder: (context, isPublic, child) {
-              return Center(
-                child: WishlistToggleSwitch(
-                  current: isPublic,
-                  onChanged: (value) => widget.visibilityNotifier.value = value,
-                  trueLabel: l10n.public,
-                  falseLabel: l10n.private,
-                  trueIcon: const Icon(
-                    Icons.lock_open,
-                    color: AppColors.makara,
-                  ),
-                  falseIcon: const Icon(
-                    Icons.lock,
-                    color: AppColors.makara,
-                  ),
-                ),
-              );
-            },
-          ),
-        ],
-      ),
+          ],
+        ),
+      ],
     );
   }
 }
@@ -94,22 +60,14 @@ class _CreateDialogContentState extends State<_CreateDialogContent> {
 Future<void> showCreateDialog(BuildContext context, WidgetRef ref) async {
   final l10n = context.l10n;
   final nameController = TextEditingController();
-  final visibilityNotifier = ValueNotifier<bool>(false);
   final userId = ref.read(supabaseClientProvider).auth.currentUserNonNull.id;
 
   return showAppDialog(
     context,
     title: l10n.createWishlist,
-    content: _CreateDialogContent(
-      nameController: nameController,
-      visibilityNotifier: visibilityNotifier,
-    ),
+    content: _CreateDialogContent(nameController: nameController),
     confirmButtonLabel: l10n.createButton,
     onConfirm: () async {
-      final visibility = visibilityNotifier.value
-          ? WishlistVisibility.public
-          : WishlistVisibility.private;
-
       await ref.read(wishlistMutationsProvider.notifier).create(
             WishlistCreateRequest(
               name: nameController.text,
@@ -119,7 +77,6 @@ Future<void> showCreateDialog(BuildContext context, WidgetRef ref) async {
                   .read(wishlistServiceProvider)
                   .getNextWishlistOrderByUser(userId),
               updatedBy: userId,
-              visibility: visibility,
             ),
           );
     },
