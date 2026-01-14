90b01f1813c312923704ab7ffe72fc0698d4faf3
Tanguy Mossion <tanguy.mossion@theodo.com>
2026-01-14 16:48:40 +0100

    test: update home screen golden test

--- wishlist/test/modules/home/view/home_screen_test.dart
+++ wishlist/test/modules/home/view/home_screen_test.dart
@@ -1,6 +1,8 @@
 import 'package:adaptive_test/adaptive_test.dart';
 import 'package:wishlist/modules/home/view/home_screen.dart';
 import 'package:wishlist/shared/navigation/floating_nav_bar_navigator.dart';
+
+import '../../../fixtures/fake_providers.dart';
 import '../../../pump_app.dart';
 
 void main() {
@@ -13,8 +15,26 @@ void main() {
         currentTab: FloatingNavBarTab.home,
       ),
       windowConfig: variant,
+      overrides: homeScreenOverrides(),
     );
 
+    await tester.pumpAndSettle();
+
     await tester.expectGolden<HomeScreen>(variant);
+  });
+
+  testAdaptiveWidgets('$HomeScreen when empty', (tester, variant) async {
+    await tester.pumpApp(
+      const FloatingNavBarNavigator(
+        // ignore: avoid_redundant_argument_values
+        currentTab: FloatingNavBarTab.home,
+      ),
+      windowConfig: variant,
+      overrides: homeScreenOverrides(wishlists: []),
+    );
+
+    await tester.pumpAndSettle();
+
+    await tester.expectGolden<HomeScreen>(variant, suffix: 'empty');
   });
 }
